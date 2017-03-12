9f79a8056a30b28e4139dd21a8f3483b7785b085
bradytheinventor <bcra1234@gmail.com>
2017-03-12 23:51:27 +0000

    Moved plot functions to separate module
    Moved fundamental plotting functions to a separate module file, soon all patterns will be in separate files to make working with them easier.

--- LEDcube.py
+++ LEDcube.py
@@ -1,46 +1,11 @@
 #import core functions
 from LEDcubeCore import *
+from patterns.plot import *
 from math import sin, sqrt
 
 """----DECLARE VARIABLE----"""
 threadRunning = True
 
-"""----------------PLOT FUNCTIONS---------------"""
-#sets a specific LED high or low
-def plot(x, y, z, n=1):
-    points[y][x][z] = n
-
-#fills from (x1,y1,z1) to (x2,y2,z2) with n
-def plotFill(x1, y1, z1, x2, y2, z2, n=1):
-    #if point2 > point1, swap them
-    if x1 > x2:
-        x2Old = x2
-        x2 = x1
-        x1 = x2Old
-    if y1 > y2:
-        y2Old = y2
-        y2 = y1
-        y1 = y2Old
-    if z1 > z2:
-        z2Old = z2
-        z2 = z1
-        z1 = z2Old
-    for y in range(y1, y2+1):
-        for x in range(x1, x2+1):
-            for z in range(z1, z2+1):
-                plot(x, y, z, n)
-
-#a quick and easy way to use plotRect to light the whole cube
-def fullcube(n=1):
-    plotFill(0, 0, 0, 3, 3, 3, n)
-
-#and a quick way to clear the whole cube
-def clear(n=0):
-    plotFill(0, 0, 0, 3, 3, 3, n)
-
-#just in case you like camelCase, this runs the same as fullcube
-def fullCube(n=1):
-    fullcube(n)
 
 """-------------------PATTERNS-------------------"""
 #spins a plane around the specified axis
---
+++ patterns/plot.py
@@ -0,0 +1,46 @@
+#this file contains the fundamental plotting functions; all other patterns
+#   should use these to write to the 'points' list
+
+#import points list
+from LEDcubeCore import points
+
+#sets a specific LED high or low
+def plot(x, y, z, n=1):
+    points[y][x][z] = n
+
+#fills from (x1,y1,z1) to (x2,y2,z2) with n
+def plotFill(x1, y1, z1, x2, y2, z2, n=1):
+    #if point1 > point2, swap them
+    if x1 > x2:
+        x2Old = x2
+        x2 = x1
+        x1 = x2Old
+        
+    if y1 > y2:
+        y2Old = y2
+        y2 = y1
+        y1 = y2Old
+        
+    if z1 > z2:
+        z2Old = z2
+        z2 = z1
+        z1 = z2Old
+
+    #fill in points
+    for y in range(y1, y2+1):
+        for x in range(x1, x2+1):
+            for z in range(z1, z2+1):
+                plot(x, y, z, n)
+
+#a quick and easy way to use plotRect to light the whole cube
+def fullcube(n=1):
+    plotFill(0, 0, 0, 3, 3, 3, n)
+
+#and an overloaded version for those who like consistent camelCase
+def fullCube(n=1):
+    fullcube(n)
+
+#and a quick way to clear the whole cube
+def clear(n=0):
+    plotFill(0, 0, 0, 3, 3, 3, n)
+    
