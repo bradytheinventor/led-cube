ab1ab172c8a6dfa4246170b54ea7b855c011c36b
bradytheinventor <bcra1234@gmail.com>
2017-03-12 23:22:47 +0000

    Improved core documentation
    Added more descriptive comments to better document what the core classes do.

--- LEDcubeCore.py
+++ LEDcubeCore.py
@@ -38,6 +38,7 @@ points = [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
           [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]]
 
 """-------------CLASS DEFINITIONS----------------"""
+#this class handles interactions between the code and the physical shift registers
 class ShiftRegister():
     def __init__(self, datapin, clockpin, latchpin):
         self.datapin = datapin
@@ -96,28 +97,35 @@ class threadRunClass():
     def __init__(self, running):
         self.running = running
 
-#essential part of the code, this takes a 3D list and outputs it to the cube
+#this class runs in a separate thread, continuously reading the 'points' list
+#   and writing its values to the LED cube
 class Multiplexer():
     def __init__(self):
         print("Initializing multiplexer...")
+        
         self.running = True
         self.register1 = ShiftRegister(10, 12, 13)
         self.register2 = ShiftRegister(22, 16, 18)
 
-    #the second parameter has to exist, but is ignored by my code,
-    #   so I just gave it a fun strange name
-    def multiplex(self, fluggamuggachuggahugga):
+    #the second parameter has to exist for the thread to run, but does nothing
+    def multiplex(self, p):
         while self.running:
             for y in range(len(points)):
+                #turn previous layer off before updating LEDs
                 GPIO.output(transistors[y-1], 0)
+                
                 for x in range(len(points[y])):
                     for z in range(len(points[y][x])):
+                        #pick which register to write to - each controls half
+                        #   of the cube
                         if x < 2:
                             self.register2.clock(points[y][x][z])
                         else:
                             self.register1.clock(points[y][x][z])
-                            
+
+                #write register values to cube and enable layer
                 self.register1.latch()
                 self.register2.latch()
                 GPIO.output(transistors[y], 1)
+                
                 sleep(0.001)
